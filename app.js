091f2af6a996e8897e8b7f3985c9c1c2dd838f7d
NicholasBokeria <nikolozbokeria01@gmail.com>
2018-12-20 23:14:51 +0400

    Setup linter and prettir

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.set('view engine', 'ejs')
+
+const arr = ['Hello', 'World', 'Test']
+
+app.get('/', (req, res) => res.render('index', {
+    arr
+}))
+
+app.get('/create', (req, res) => res.render('create'))
+app.post('/create', (req, res) => {
+    arr.push(req.body.text)
+    res.redirect('/')
 })
 
 app.listen(3000, () => {
