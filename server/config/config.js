8eb48b7132455429a417e8a15a0f0b2657aced5b
NicholasBokeria <nikolozbokeria01@gmail.com>
2018-12-21 23:10:38 +0400

    Fixing env error

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+const env = process.env.NODE_ENV || 'development'
+console.log('env *********', env)
+
+if (env === 'development' || env === 'test') {
+    let config = require('./config.json')
+    var envConfig = config[env]
+    
+    Object.keys(envConfig).forEach(key => {
+        process.env[key] = envConfig[key]
+    })
+}
