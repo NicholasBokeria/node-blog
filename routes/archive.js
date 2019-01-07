37a8876226a981c62bd8bd2d0434b16926ab56cb
NicholasBokeria <nikolozbokeria01@gmail.com>
2019-01-07 22:04:45 +0400

    Added the post page

--- routes/archive.js
+++ routes/archive.js
@@ -37,4 +37,36 @@ router.get('/archive/:page', (req, res) => {
     showPosts(req, res)
 })
 
+
+router.get('/posts/:post', (req, res, next) => {
+    const url = req.params.post.trim().replace(/ +(?= )/g, '')
+    const userId = req.session.userId
+    const userLogin = req.session.userLogin
+
+    if (!url) {
+        const err = new Error('not found')
+        err.status = 404
+        next(err)
+    } else {
+        Post.findOne({
+            url
+        }).then(post => {
+            if (!post) {
+                const err = new Error('not found')
+                err.status = 404
+                next(err)
+            } else {
+                res.render('post/post', {
+                    post,
+                    user: {
+                        id: userId,
+                        login: userLogin
+                    }
+                })
+            }
+        })
+    }
+})
+
+
 module.exports = router
